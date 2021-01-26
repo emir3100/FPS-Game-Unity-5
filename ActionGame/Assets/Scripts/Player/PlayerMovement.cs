639da5aa8889700cee21b39211ebc759ff290363
Emir Jasarevic <emir2000@hotmail.se>
2021-01-26 17:31:31 +0100

    Created player movement

---
+++ ActionGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,232 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [Header("Assingables")]
+    public Transform playerCam;
+    public Transform orientation;
+
+    private Rigidbody rb;
+
+    private float xRotation;
+    private float sensitivity = 50f;
+    private float sensMultiplier = 1f;
+
+    [Header("Movement")]
+    public float moveSpeed = 4500;
+    public float maxSpeed = 20;
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    public float counterMovement = 0.175f;
+    private float threshold = 0.01f;
+    public float maxSlopeAngle = 35f;
+
+    [Header("Slide")]
+    public float slideForce = 400;
+    public float slideCounterMovement = 0.2f;
+
+    private Vector3 crouchScale = new Vector3(1, 0.5f, 1);
+    private Vector3 playerScale;
+
+    [Header("Jumping")]
+    public float jumpForce = 550f;
+
+    private bool readyToJump = true;
+    private float jumpCooldown = 0.25f;
+
+    float x, y;
+    bool jumping, sprinting, crouching;
+
+    private Vector3 normalVector = Vector3.up;
+    private Vector3 wallNormalVector;
+
+    private float desiredX;
+    private bool cancellingGrounded;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void Start() {
+        playerScale =  transform.localScale;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    
+    private void FixedUpdate() {
+        Movement();
+    }
+
+    private void Update() {
+        MyInput();
+        Look();
+    }
+
+    private void MyInput() {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+        jumping = Input.GetButton("Jump");
+        crouching = Input.GetKey(KeyCode.LeftControl);
+      
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            StartCrouch();
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+            StopCrouch();
+    }
+
+    private void StartCrouch() {
+        transform.localScale = crouchScale;
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+        if (rb.velocity.magnitude > 0.5f) {
+            if (grounded) {
+                rb.AddForce(orientation.transform.forward * slideForce);
+            }
+        }
+    }
+
+    private void StopCrouch() {
+        transform.localScale = playerScale;
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+    }
+
+    private void Movement() {
+        rb.AddForce(Vector3.down * Time.deltaTime * 10);
+        
+        Vector2 mag = FindVelRelativeToLook();
+        float xMag = mag.x, yMag = mag.y;
+
+        CounterMovement(x, y, mag);
+        
+        if (readyToJump && jumping) Jump();
+
+        float maxSpeed = this.maxSpeed;
+        
+        if (crouching && grounded && readyToJump) {
+            rb.AddForce(Vector3.down * Time.deltaTime * 3000);
+            return;
+        }
+        
+        if (x > 0 && xMag > maxSpeed) x = 0;
+        if (x < 0 && xMag < -maxSpeed) x = 0;
+        if (y > 0 && yMag > maxSpeed) y = 0;
+        if (y < 0 && yMag < -maxSpeed) y = 0;
+
+        float multiplier = 1f, multiplierV = 1f;
+        
+        if (!grounded) {
+            multiplier = 0.5f;
+            multiplierV = 0.5f;
+        }
+        
+        if (grounded && crouching) multiplierV = 0f;
+
+        rb.AddForce(orientation.transform.forward * y * moveSpeed * Time.deltaTime * multiplier * multiplierV);
+        rb.AddForce(orientation.transform.right * x * moveSpeed * Time.deltaTime * multiplier);
+    }
+
+    private void Jump() {
+        if (grounded && readyToJump) {
+            readyToJump = false;
+
+            rb.AddForce(Vector2.up * jumpForce * 1.5f);
+            rb.AddForce(normalVector * jumpForce * 0.5f);
+
+            Vector3 vel = rb.velocity;
+            if (rb.velocity.y < 0.5f)
+                rb.velocity = new Vector3(vel.x, 0, vel.z);
+            else if (rb.velocity.y > 0) 
+                rb.velocity = new Vector3(vel.x, vel.y / 2, vel.z);
+            
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+    
+    private void ResetJump() {
+        readyToJump = true;
+    }
+    
+    private void Look() {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+
+        Vector3 rot = playerCam.transform.localRotation.eulerAngles;
+        desiredX = rot.y + mouseX;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCam.transform.localRotation = Quaternion.Euler(xRotation, desiredX, 0);
+        orientation.transform.localRotation = Quaternion.Euler(0, desiredX, 0);
+    }
+
+    private void CounterMovement(float x, float y, Vector2 mag) {
+        if (!grounded || jumping) return;
+
+        if (crouching) {
+            rb.AddForce(moveSpeed * Time.deltaTime * -rb.velocity.normalized * slideCounterMovement);
+            return;
+        }
+
+        if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) || (mag.x > threshold && x < 0)) {
+            rb.AddForce(moveSpeed * orientation.transform.right * Time.deltaTime * -mag.x * counterMovement);
+        }
+        if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0) || (mag.y > threshold && y < 0)) {
+            rb.AddForce(moveSpeed * orientation.transform.forward * Time.deltaTime * -mag.y * counterMovement);
+        }
+
+        if (Mathf.Sqrt((Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2))) > maxSpeed) {
+            float fallspeed = rb.velocity.y;
+            Vector3 n = rb.velocity.normalized * maxSpeed;
+            rb.velocity = new Vector3(n.x, fallspeed, n.z);
+        }
+    }
+
+    public Vector2 FindVelRelativeToLook() {
+        float lookAngle = orientation.transform.eulerAngles.y;
+        float moveAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
+
+        float u = Mathf.DeltaAngle(lookAngle, moveAngle);
+        float v = 90 - u;
+
+        float magnitue = rb.velocity.magnitude;
+        float yMag = magnitue * Mathf.Cos(u * Mathf.Deg2Rad);
+        float xMag = magnitue * Mathf.Cos(v * Mathf.Deg2Rad);
+        
+        return new Vector2(xMag, yMag);
+    }
+
+    private bool IsFloor(Vector3 v) {
+        float angle = Vector3.Angle(Vector3.up, v);
+        return angle < maxSlopeAngle;
+    }
+
+    private void OnCollisionStay(Collision collision) {
+        int layer = collision.gameObject.layer;
+        if (whatIsGround != (whatIsGround | (1 << layer))) return;
+
+        for (int i = 0; i < collision.contactCount; i++) {
+            Vector3 normal = collision.contacts[i].normal;
+            //FLOOR
+            if (IsFloor(normal)) {
+                grounded = true;
+                cancellingGrounded = false;
+                normalVector = normal;
+                CancelInvoke(nameof(StopGrounded));
+            }
+        }
+
+        float delay = 3f;
+        if (!cancellingGrounded) {
+            cancellingGrounded = true;
+            Invoke(nameof(StopGrounded), Time.deltaTime * delay);
+        }
+    }
+
+    private void StopGrounded() {
+        grounded = false;
+    }
+    
+}
---
+++ ActionGame/Assets/Scripts/Player/WallRunning.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRunning : MonoBehaviour
+{
+    [SerializeField]
+    private float upForce;
+    [SerializeField]
+    private float sideForce;
+
+    private Rigidbody rb;
+
+    public Transform head;
+    public Transform cam;
+
+    private bool isLeft;
+    private bool isRight;
+
+    private float distFromLeft;
+    private float distFromRight;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        wallChecker();
+    }
+
+    private void wallChecker()
+    {
+        RaycastHit leftWall;
+        RaycastHit rightWall;
+        if(Physics.Raycast(head.position, head.right, out rightWall))
+        {
+            distFromRight = Vector3.Distance(head.position, rightWall.point);
+            if(distFromRight < 3f)
+            {
+                isRight = true;
+                isLeft = false;
+            }
+        }
+
+        if (Physics.Raycast(head.position, -head.right, out leftWall))
+        {
+            distFromLeft = Vector3.Distance(head.position, leftWall.point);
+            if (distFromLeft < 3f)
+            {
+                isRight = false;
+                isLeft = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("RunnableWall"))
+        {
+            rb.useGravity = false;
+
+            if (isRight)
+            {
+                cam.localEulerAngles = new Vector3(0f, 0f, 10f);
+            }
+
+            if (isLeft)
+            {
+                cam.localEulerAngles = new Vector3(0f, 0f, -10f);
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.CompareTag("RunnableWall"))
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (isLeft)
+                {
+                    rb.AddForce(Vector3.up * upForce * Time.deltaTime);
+                    rb.AddForce(head.right * sideForce * Time.deltaTime);
+                }
+                if (isRight)
+                {
+                    rb.AddForce(Vector3.up * upForce * Time.deltaTime);
+                    rb.AddForce(-head.right * sideForce * Time.deltaTime);
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.CompareTag("RunnableWall"))
+        {
+            rb.useGravity = true;
+            cam.localEulerAngles = new Vector3(0f, 0f, 0f);
+        }
+    }
+}
