3abbb51f3dd12cf4378b5ffc7fb05ee85210296a
Emir Jasarevic <emir2000@hotmail.se>
2021-02-12 22:40:14 +0100

    added assets

---
+++ ActionGame/Assets/Scripts/Enviroment/JumpPad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            var rigidbody = collision.collider.GetComponent<Rigidbody>();
+            AddForce(rigidbody);
+        }
+    }
+
+    private void AddForce(Rigidbody rigidbody)
+    {
+
+        if (rigidbody != null)
+        {
+            rigidbody.AddForce(Vector3.up * 4000f);
+            FindObjectOfType<AudioManager>().Play("JumpGun");
+        }
+    }
+}
